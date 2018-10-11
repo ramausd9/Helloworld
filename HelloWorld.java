ce3ea34180f28cc213bacca0abfd821cac67bb7a
vagrant <vagrant@vagrant.vm>
2018-10-11 07:29:02 +0000

    Hellow World app added

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        int i=1;
+	while(i<=10)
+	{
+		System.out.println(i+".Hello, World");
+		i++;
+	}
+
+    }
+
+}
